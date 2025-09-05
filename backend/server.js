f6cefd8853fd92ff630fa6c15b991b3829673c23
lexicorelabssystem <lexicorelabssystem@gmail.com>
2025-09-05 01:40:20 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,12 +14,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
-  sl: { rejectUnauthorized: false },
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // obligatorio para PostgreSQL en Render
 });
 
 
