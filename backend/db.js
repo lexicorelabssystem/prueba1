af9d28b1281db5dcacf446fba621d2505a815a64
lexicorelabssystem <lexicorelabssystem@gmail.com>
2025-09-05 01:31:26 -0400

    Update db.js
    cambio para base de datos de render

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,11 @@
-const { Pool } = require('pg'); // Importa el módulo 'pg' para conectar con PostgreSQL
-require('dotenv').config(); // Carga las variables de entorno desde el archivo .env
+const { Pool } = require('pg');
+require('dotenv').config(); // Carga las variables desde .env o desde Render
 
-// Configuración de la conexión a PostgreSQL
 const pool = new Pool({
-  user: process.env.DB_USER,     // Usuario de la base de datos
-  host: process.env.DB_HOST,     // Host, normalmente localhost
-  database: process.env.DB_NAME, // Nombre de la base de datos
-  password: process.env.DB_PASS, // Contraseña
-  port: process.env.DB_PORT,     // Puerto (5432 por defecto)
+  connectionString: process.env.DATABASE_URL, // Esto funciona con Render
+  ssl: {
+    rejectUnauthorized: false, // Necesario en Render para PostgreSQL
+  },
 });
 
-module.exports = pool; // Exporta la conexión para usarla en otros archivos
-
-
+module.exports = pool;
