47967438ce5d25b2659d9ce0fa69d1199b85f962
lexicorelabssystem <lexicorelabssystem@gmail.com>
2025-09-06 00:08:20 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -243,4 +243,6 @@ app.get("/api/descargar/word", async (req, res) => {
 
 // Servidor
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`✅ Servidor corriendo en http://localhost:${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Servidor corriendo en el puerto ${PORT}`);
+});
